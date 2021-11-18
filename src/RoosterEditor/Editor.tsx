790fe4be756856e3698586f02e9f5eb66750212f
FAREAST\sayanpandey <sayanpandey@microsoft.com>
2021-11-19 04:20:07 +0530

    Able to get functional buttons

--- src/RoosterEditor/Editor.tsx
+++ src/RoosterEditor/Editor.tsx
@@ -1,22 +1,37 @@
-import { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { IEditor } from "roosterjs-editor-types";
 import { getEditor } from "./rooster";
+import * as roosterjs from "roosterjs";
 
 export const Editor = () => {
   const editorRef = useRef<HTMLDivElement>(null);
+  var editor: IEditor;
+
   useEffect(() => {
-    getEditor(editorRef);
+    editor = getEditor(editorRef);
   });
 
   return (
-    <div
-      ref={editorRef}
-      id="editorDiv"
-      style={{
-        width: " 500px",
-        height: " 300p",
-        overflow: " auto",
-        border: " solid 1px black",
-      }}
-    ></div>
+    <React.Fragment>
+      <div
+        ref={editorRef}
+        id="editorDiv"
+        style={{
+          width: "500px",
+          height: "300px",
+          overflow: "auto",
+          border: "solid 1px black",
+        }}
+      ></div>
+      <button id="buttonB" onClick={() => roosterjs.toggleBold(editor!)}>
+        B
+      </button>
+      <button id="buttonI" onClick={() => roosterjs.toggleItalic(editor!)}>
+        I
+      </button>
+      <button id="buttonU" onClick={() => roosterjs.toggleUnderline(editor!)}>
+        U
+      </button>
+    </React.Fragment>
   );
 };
--- src/RoosterEditor/rooster.ts
+++ src/RoosterEditor/rooster.ts
@@ -1,8 +1,12 @@
 import React, { RefObject } from "react";
+import { IEditor } from "roosterjs-editor-types";
 import * as roosterjs from "roosterjs";
 
-export const getEditor = (elementRef: RefObject<HTMLDivElement>) => {
-  console.log(elementRef);
+export const getEditor = (elementRef: RefObject<HTMLDivElement>): IEditor => {
   var editor = roosterjs.createEditor(elementRef.current!);
-  editor.setContent("Welcome to <b>RoosterJs</b>!");
+  editor.setContent(
+    "This is some random text. Hello, " +
+      "I am Sayan Pandey, This editor is great works well for now."
+  );
+  return editor;
 };
